aa2ac11967cff2b78a04284bcb05f45f48881ca1
bblike <1220047112@11.com>
2023-10-13 10:52:59 +0100

    sef

---
+++ main.py
@@ -0,0 +1,24 @@
+# Numpy, Matplotlib and Pillow was used. Remember to import.
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import cv2 as cv
+import sys
+
+__name__ = '__main__'
+
+
+def main(x):
+    return x
+
+
+print(1)
+
+
+im = cv.imread("CIMG9548.JPG")
+print(type(im))
+print(im)
+print()
+cv.imshow("display window", im)
+while True:
+    x=1
