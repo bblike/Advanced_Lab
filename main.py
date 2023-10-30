8a62468f2af1a93234c2b716c581f83cbdecd10c
bblike <lzjgnh2002@gmail.com>
2023-10-30 16:53:34 +0000

    improve over the multiprocessing and multithreading calculation

--- main.py
+++ main.py
@@ -1,125 +1,127 @@
 # Numpy, Matplotlib and Pillow was used. Remember to import.
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2 as cv
+import matplotlib.pyplot as plt
 import sys
 import multiprocessing as mp
 import datetime
 import math
+import time
 import rgb2gray
 import interactive as inte
 from PIL import Image
-
-
-def train_on_parameter(name, param):
-    result = 0
-    for num in param:
-        result += math.sqrt(num * math.tanh(num) / math.log2(num) / math.log10(num))
-    return {name: result}
-
-
-def calculation():
-    for i in range(0, raw_structure[0] - 1):
-        for j in range(0, raw_structure[1] - 1):
-            for k in range(0, 2):
-                raw[i][j][k] = np.std([im[i][j][k], im[i][j + 1][k], im[i + 1][j][k], im[i + 1][j + 1][k]])
-
-                print("process{}, {}, {} done.".format(i, j, k))
-
-
-def calculation_r(param):
-    print("holding calculations of ",len(param))
-    for i in range(0, raw_structure[0] - 1):
-        for j in param:
-            for k in [0]:
-                raw_r[i][j] = np.std([im[i][j][k], im[i][j + 1][k], im[i + 1][j][k], im[i + 1][j + 1][k]])
-                print("process{}, {}, {} done.".format(i, j, k))
-
-
-def calculation_empty(param):
-    print("holding calculations of ",len(param))
-    result = []
-    for i in range(0, raw_structure[0] - 1):
-        for j in param:
-            for k in [0]:
-                result = result + np.std([im[i][j][k], im[i][j + 1][k], im[i + 1][j][k], im[i + 1][j + 1][k]])
-                print("process{}, {}, {} done.".format(i, j, k))
+import threading as td
+from queue import Queue
+#
 
 
 
+path = "CIMG3813.JPG"
 
-
-def calculation_matlab(image):
-    print(np.shape(image))
-    slope_x, slope_y = np.gradient(image)
-    _field = np.sqrt(slope_x**2 + slope_y**2)
-    return _field
-
-
-#introducing the picture
-path = "CIMG9548-1.JPG"
+#global im
 im = cv.imread(path)
-print(np.shape(im))
+#global structure
 structure = np.shape(im)
+print(structure)
 
-#cv.imshow("display window", im)
-#cv.waitKey(1000)
-#cv.destroyAllWindows()
-
-raw = np.zeros(structure)
-raw_r = np.zeros(structure)
-raw_structure = np.array([structure[0] - 1, structure[1] - 1, 3])
-div = int(structure[1] / 8)
-raw_empty = []
+#global red_structure
+red_structure = np.array([structure[0], structure[1], 3])
+#global red
+red = np.zeros(red_structure)
 
 
-inte.create_window(path)
+
+result = []
+#calculation_red()
 
 
-"""
+
+
+def calculation_red(q):
+    for i in range(0, red_structure[0]):
+        for j in range(0, red_structure[1]):
+
+            red[i][j][1] = 150
+
+        print("process{} done.".format(i))
+    q.put(1)
+
+
+
+def calculation_red_param(param):
+
+    for i in param:
+        for j in range(0, red_structure[1]):
+
+            red[i][j][0] = 150
+
+
+        print("process {} done.".format(i))
+    print("calculation {} down".format(param))
+    print("test: ", red[(param[0])][100][0])
+def parallel():
+    begin_time = time.time()
+    n = structure[0]
+    procs = []
+    n_cpu = int(mp.cpu_count()/2)
+    chunk_size = int(n/n_cpu)
+    x = 0
+    for i in range(0, n_cpu):
+        min_i = chunk_size * i
+
+        if i < n_cpu-1:
+            max_i = chunk_size * (i+1)
+        else:
+            max_i = n
+        digits = []
+        for digit in range(min_i, max_i):
+            digits.append(digit)
+        print(digits)
+        print(len(digits))
+        procs.append(mp.Process(target=calculation_red_param, args = ([digits])))
+    for proc in procs:
+        proc.start()
+    for proc in procs:
+        proc.join()
+
+    end_time = time.time()
+    print("time spend", end_time-begin_time)
+
+
+def threading_job():
+    q = Queue()
+    ts = []
+    begin_time = time.time()
+    for i in range(0, 4):
+        t =mp.Process(target = calculation_red, args = (q,))
+        t.start()
+        ts.append(t)
+
+    for i in ts:
+        i.join()
+    results = []
+    for i in range(0,4):
+        results.append(q.get())
+    end_time = time.time()
+    print("all done")
+    print("results=", results)
+    print("time_spent=", end_time-begin_time)
+
+
+
 if __name__ == '__main__':
 
-    start_t = datetime.datetime.now()
-
-    num_cores = int(mp.cpu_count())
-    print("本地计算机有: " + str(num_cores) + " 核心")
-    pool = mp.Pool(num_cores)
-    param_dict = {'task1': list(range(0, div)),
-                  'task2': list(range(div + 1, div * 2)),
-                  'task3': list(range(div * 2 + 1, div * 3)),
-                  'task4': list(range(div * 3, div * 4)),
-                  'task5': list(range(div * 4 + 1, div * 5)),
-                  'task6': list(range(div * 5 + 1, div * 6)),
-                  'task7': list(range(div * 6 + 1, div * 7)),
-                  'task8': list(range(div * 7 + 1, div * 8))}
-    results = [pool.apply_async(calculation_r, args=param) for param in param_dict.items()]
-    results = [p.get() for p in results]
-
-    end_t = datetime.datetime.now()
-    elapsed_sec = (end_t - start_t).total_seconds()
-    print("多进程计算 共消耗: " + "{:.2f}".format(elapsed_sec) + " 秒")
-
-cv.imshow("new window", raw_r)
-cv.waitKey(1000000)
-cv.destroyAllWindows()
-cv.imwrite("raw-r.jpg", raw_r)
-cv.imwrite("raw-r.txt", raw_r)
+    parallel()
+    #threading_job()
+    print("waiting...")
+    cv.waitKey(3000)
+    cv.imshow("new window", red)
+    cv.waitKey(2000)
+    cv.destroyAllWindows()
+    cv.imwrite("raw-r.jpg", red)
 
 
-results = np.zeros([3, np.shape(im)[0], np.shape(im)[1]])
-for k in range(0,3):
-
-    results[k] = calculation_matlab(im[:,:,k])
-    print(results[k])
-    if k == 1:
-        cv.imshow("original{}".format(k), im[:,:,k])
 
 
-        cv.imshow("test{}".format(k), results[k])
 
-        cv.imwrite("test{}.jpg".format(k), results[k])
 
-while True:
-    cv.waitKey(1000000)
-cv.destroyAllWindows()
-"""
