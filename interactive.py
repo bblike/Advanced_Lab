1a872d4142917bc4a3edbc222552807b09c609a9
bblike <lzjgnh2002@gmail.com>
2023-11-03 16:30:03 +0000

    updates of methods of analysis

--- interactive.py
+++ interactive.py
@@ -4,31 +4,46 @@ import tkinter
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
+start_point_0 = 0
+mid_point_0 = 0
+start_point_1 = 0
+mid_point_1 = 0
+
+
+def fresh_window(e11, e12, e21, e22):
+    start_point1_0 = e11.get()
+    mid_poin_0 = e12.get()
+    start_point_1 = e21.get()
+    mid_point_1 = e22.get()
+
 
 def create_window(myImage):
     root = Tk()
     root.title("select the analyse points")
-    root.geometry('600x600')
+    root.geometry('800x400')
 
     im = Image.open(myImage)
-    photo = ImageTk.PhotoImage(im)
+    photo = ImageTk.PhotoImage(im.resize([400,400]))
     imlabel = Label(image=photo)
-    imlabel.grid(row=0, column=2, columnspan=2, rowspan=2, sticky = S+N)
-    Label(root, text="start point").grid(row=1)
-    Label(root, text="mid point").grid(row=2)
+    imlabel.grid(row=0, column=3, columnspan=1, rowspan=5, sticky=W + N)
+    Label(root, text="start point").grid(row=1, column=0)
+    Label(root, text="mid point").grid(row=2, column=0)
 
-    e1 = Entry(root)
-    e2 = Entry(root)
-    yes = ttk.Frame(root, padding=10)
-    e1.grid(row=1, column=1)
-    e2.grid(row=2, column=1)
-    yes.grid()
-    ttk.Button(yes, text = "Yes", command=root.destroy).grid(row=3, column=1)
+    fresh = ttk.Frame(root, padding=0)
+    e11 = Entry(root)
+    e12 = Entry(root)
+    e21 = Entry(root)
+    e22 = Entry(root)
+    yes = ttk.Frame(root, padding=0)
+    e11.grid(row=1, column=1)
+    e12.grid(row=1, column=2)
+    e21.grid(row=2, column=1)
+    e22.grid(row=2, column=2)
+    fresh.grid(row=3, column=1)
+    yes.grid(row=4, column=1)
+    ttk.Button(fresh, text="fresh", command=fresh_window(e11, e12, e21, e22)).grid(row=3, column=1)
+    ttk.Button(yes, text="Yes", command=root.destroy).grid(row=4, column=1)
 
-
+    p1 = Canvas()
 
     root.mainloop()
-
-
-
-
--- main.py
+++ main.py
@@ -7,58 +7,42 @@ import multiprocessing as mp
 import datetime
 import math
 import time
-import rgb2gray
-import interactive as inte
+
+
 from PIL import Image
 import threading as td
 from queue import Queue
+import os
+import xlwt
 #
 
 
-
-path = "CIMG3813.JPG"
-
-#global im
-im = cv.imread(path)
-#global structure
-structure = np.shape(im)
-print(structure)
-
-#global red_structure
-red_structure = np.array([structure[0], structure[1], 3])
-#global red
-red = np.zeros(red_structure)
-
-
-
-result = []
-#calculation_red()
-
-
-
+def rgb2gray(rgb):
+    gray = np.mean(rgb, -1)
+    return gray
 
 def calculation_red(q):
+    temp = np.full((structure[0],structure[1]), 0)
     for i in range(0, red_structure[0]):
         for j in range(0, red_structure[1]):
-
-            red[i][j][1] = 150
+            temp[i][j] = im[i][j][0]
+            #red[i][j][1] = im[i][j][0]
 
         print("process{} done.".format(i))
-    q.put(1)
-
+    q.put(temp)
 
 
 def calculation_red_param(param):
-
+    temp = []
     for i in param:
         for j in range(0, red_structure[1]):
 
-            red[i][j][0] = 150
+            red[i][j][0] = im[i][j][0]
 
 
         print("process {} done.".format(i))
     print("calculation {} down".format(param))
-    print("test: ", red[(param[0])][100][0])
+
 def parallel():
     begin_time = time.time()
     n = structure[0]
@@ -93,32 +77,113 @@ def threading_job():
     ts = []
     begin_time = time.time()
     for i in range(0, 4):
-        t =mp.Process(target = calculation_red, args = (q,))
+        t = td.Thread(target=calculation_red, args=(q,))
         t.start()
         ts.append(t)
 
     for i in ts:
         i.join()
     results = []
-    for i in range(0,4):
-        results.append(q.get())
+
+    results.append(q.get())
     end_time = time.time()
     print("all done")
     print("results=", results)
     print("time_spent=", end_time-begin_time)
+    return results
 
-
-
-if __name__ == '__main__':
-
-    parallel()
-    #threading_job()
-    print("waiting...")
-    cv.waitKey(3000)
-    cv.imshow("new window", red)
+def print_image(x):
+    cv.waitKey(5000)
+    cv.imshow("new window", x)
     cv.waitKey(2000)
     cv.destroyAllWindows()
-    cv.imwrite("raw-r.jpg", red)
+    cv.imwrite("raw-r.jpg", x)
+
+
+
+
+
+
+path = r"C:\Users\Li Zhejun\Desktop\Advanced_Lab\thursdayw4"
+files = os.listdir(path)
+#path = "newtest.JPG"
+file_length = len(files)
+selector = [100, 200, 300, 400]
+excel_result = []
+if __name__ == '__main__':
+    for file in files:
+        for i in selector:
+            for j in range(0 ,16):
+                if file == "{}P{}.JPG".format(i, j):
+                    im = cv.imread(r"thursdayw4\{}".format(file))
+                    # global structure
+                    structure = np.shape(im)
+                    temp = rgb2gray(im)
+                    print(structure)
+                    result = []
+                    # global red_structure
+                    red_structure = np.array([structure[0], structure[1], 3])
+                    # global red
+                    red = np.zeros(red_structure)
+
+                    # parallel()
+                    #temp = threading_job()
+                    print(temp)
+                    print("structure of temp is ", np.shape(temp))
+                    print("waiting...")
+                    #temp1 = np.array(temp[0])
+                    temp1 = np.array(temp)
+                    new_im = Image.fromarray(temp1)
+                    #new_im.show()
+                    #new_im.save("{}P{}.jpg".format(i, j))
+                    # goto the middle
+                    xpos = int(structure[0] / 2)
+                    ypos = int(structure[1] / 2)
+                    r = 12
+                    new_array = temp1[xpos - r:xpos + r, ypos - r:ypos + r]
+                    print(new_array)
+                    means = np.mean(new_array)
+                    normaldis = 0
+                    midx =  int(len(new_array)/2)
+                    midy =  int(len(new_array[0])/2)
+                    for n in range(len(new_array)):
+                        for m in range(len(new_array[n])):
+                            arg = np.sqrt((midx-n)**2 + (midy-m)**2)
+                            if arg == 0:
+                                normaldis += new_array[n][m] * 2
+                            elif arg <= 8:
+                                normaldis += new_array[n][m]/arg
+                    stds = np.std(new_array)
+                    print("mean of {}{} = ".format(i, j), means)
+                    print("standard error of {}{} =".format(i, j), stds)
+                    print("normalised result", normaldis)
+                    excel_result.append([i, j, means, stds, normaldis])
+
+    print(excel_result)
+    writer = np.array(excel_result)
+    book=xlwt.Workbook(encoding="utf-8", style_compression=0)
+    sheet = book.add_sheet('thursday', cell_overwrite_ok=True)
+    sheet.write(0, 1, "pressure")
+    sheet.write(0, 2, "angle count")
+    sheet.write(0, 3, "mean")
+    sheet.write(0, 4, "standard error")
+    sheet.write(0, 5, "normalised results")
+    for i in range(len(writer)):
+        for j in range(len(writer[0])):
+            sheet.write(i+1, j+1, "{}".format(writer[i][j]))
+
+    book.save('test.xls')
+
+
+
+
+
+
+
+
+
+
+
 
 
 
